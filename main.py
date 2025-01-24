89553d6e60b2efdca30dc528cbdf6b74e190bc99
alanleew <alanleew412@gmail.com>
2025-01-23 19:29:26 -0800

    Added comments and removed unnecessary lines

--- main.py
+++ main.py
@@ -31,15 +31,12 @@ percentage_change = float(format(price_change / yesterday_close * 100, ".2f"))
 # If percentage change > 5%, send a Whatsapp message
 if abs(percentage_change) > 0.05:
     news_param = {
-        "q": (STOCK, COMPANY_NAME),
+        "qInTitle" : COMPANY_NAME,
         "apikey": NEWS_API_KEY,
-        # "pageSize": 100,
     }
     news_request = requests.get(NEWS_ENDPOINT, params=news_param)
     news_json_data = news_request.json()
-    top_articles = news_json_data["articles"][0:3]
-    headline = [article["title"] for article in top_articles]
-    brief = [article["content"] for article in top_articles]
+    top_articles = news_json_data["articles"][:3] # Slice operator to stop at 3rd item
 
     if percentage_change > 0:
         message_body = f"{STOCK} 🔺+{percentage_change}%"
